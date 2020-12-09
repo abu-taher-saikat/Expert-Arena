651baa755229c17f219f0a5b7ed660415ebca91b
abutaher <mdabutahersaikat@gmail.com>
2020-12-09 15:41:28 +0600

    faq add

--- js/script.js
+++ js/script.js
@@ -5,3 +5,21 @@ const navLink = document.getElementById('nav-section');
 hamburger.addEventListener('click',()=>{
     navLink.classList.toggle('show');
 })
+
+
+// accrodiain part start
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+// accrodiain part end
