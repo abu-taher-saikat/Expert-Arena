54733ae2f60088fd983dcd1a3d1ffc0b6a18d97f
abutaher <mdabutahersaikat@gmail.com>
2020-12-19 01:37:49 +0600

    done

--- js/script.js
+++ js/script.js
@@ -23,3 +23,24 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 // accrodiain part end
+
+
+// trying to fix modal
+var modals = document.querySelectorAll("[data-modal]");
+
+modals.forEach(function (trigger) {
+  trigger.addEventListener("click", function (event) {
+    event.preventDefault();
+    var modal = document.getElementById(trigger.dataset.modal);
+    modal.classList.add("open");
+    var exits = modal.querySelectorAll(".modal-exit");
+    exits.forEach(function (exit) {
+      exit.addEventListener("click", function (event) {
+        event.preventDefault();
+        modal.classList.remove("open");
+      });
+    });
+  });
+});
+
+// trying to fix modal
