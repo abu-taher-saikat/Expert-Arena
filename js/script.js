53780e950eed9111438aff1071fa794b8ca976b0
abutaher <mdabutahersaikat@gmail.com>
2020-12-25 21:51:48 +0600

    a

--- js/script.js
+++ js/script.js
@@ -67,3 +67,25 @@ function initMap() {
   });
 }
 // addaing a google map
+
+
+
+
+// Setting up active button
+// Add active class to the current button (highlight it)
+// var header = document.getElementById("nav-section");
+// var btns = header.getElementsByClassName("active-class");
+// Loop through the buttons and add the active class to the current/clicked button
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function() {
+//     var current = document.getElementsByClassName("active");
+
+//     // If there's no active class
+//     if (current.length > 0) {
+//       current[0].className = current[0].className.replace(" active", "");
+//     }
+
+//     // Add the active class to the current/clicked button
+//     this.className += " active";
+//   });
+// }
