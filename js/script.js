4882f21bc1f01db0641c6559a7f4580e4bfd64a4
abutaher <mdabutahersaikat@gmail.com>
2020-12-31 01:46:18 +0600

    done

--- js/script.js
+++ js/script.js
@@ -111,3 +111,31 @@ document.getElementById('graphic-design').addEventListener('click', function() {
 document.getElementById('data-entry').addEventListener('click', function() {
   location.href = 'data-entry.html'
 }, false);
+
+
+
+
+// testimonial
+// $(document).ready(function () {
+//   $(".testimonial .indicators li").click(function () {
+//     var i = $(this).index();
+//     var targetElement = $(".testimonial .tabs li");
+//     targetElement.eq(i).addClass("active");
+//     targetElement.not(targetElement[i]).removeClass("active");
+//   });
+
+//   $(".testimonial .tabs li").click(function () {
+//     var targetElement = $(".testimonial .tabs li");
+//     targetElement.addClass("active");
+//     targetElement.not($(this)).removeClass("active");
+//   });
+// });
+// $(document).ready(function () {
+//   $(".slider .swiper-pagination span").each(function (i) {
+//     $(this)
+//       .text(i + 1)
+//       .prepend("0");
+//   });
+// });
+
+// testimonial
